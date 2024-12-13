f921ded6a55c55e28cd0c4c95072bb9719aff35d
Habibullah Sirat <1931291@iub.edu.bd>
2024-12-14 03:01:26 +0600

    some Updated

---
+++ src/main/java/mainpkg/educationboardms3/TeacherDashboardController.java
@@ -0,0 +1,106 @@
+import javafx.event.ActionEvent;
+
+public class TeacherDashboardController {
+
+    @FXML
+    void assignHomeWorkOnClick(ActionEvent event) {
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("assignHomework.fxml"));
+            Scene scene = new Scene(fxmlLoader.load();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+    }
+
+    @FXML
+    void collaborateWithTeachersOnClick(ActionEvent event) {
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("collaborate-teachers.fxml"));
+            Scene scene = new Scene(fxmlLoader.load();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+    @FXML
+    void communicateParentsOnClick(ActionEvent event) {
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("communicate-parents.fxml"));
+            Scene scene = new Scene(fxmlLoader.load();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+    }
+
+    @FXML
+    void createLessonOnClick(ActionEvent event) {
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("lesson-plan.fxml"));
+            Scene scene = new Scene(fxmlLoader.load();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+    }
+
+    @FXML
+    void gradeAssignmentOnClick(ActionEvent event) {
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("grade-assignments.fxml"));
+            Scene scene = new Scene(fxmlLoader.load();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+    }
+
+    @FXML
+    void monitorAttendanceOnClick(ActionEvent event) {
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("monitor-attendance.fxml"));
+            Scene scene = new Scene(fxmlLoader.load();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+    }
+
+    @FXML
+    void scheduleExamsOnClick(ActionEvent event) {
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("schedule-exams.fxml"));
+            Scene scene = new Scene(fxmlLoader.load();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+    }
+
+    @FXML
+    void startExamOnClick(ActionEvent event) {
+
+
+    }
+
+    @FXML
+    void studentPerformanceOnClick(ActionEvent event) {
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("student-performance.fxml"));
+            Scene scene = new Scene(fxmlLoader.load();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+    }
+
+}
